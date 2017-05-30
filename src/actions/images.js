94e73b0959066c85963d63f3be020488ace98cde
Dan Kolbman <dankolbman@gmail.com>
2017-05-29 21:31:58 -0400

    Add photopage

--- src/actions/images.js
+++ src/actions/images.js
@@ -13,12 +13,15 @@ export const RECEIVE_IMAGES = 'RECEIVE_IMAGES'
 function receiveImages(username, tripid, json) {
   if (json.total > 0) {
     var images = json.images
+    console.log(images[0])
     for (var i = 0; i < images.length; i++) {
-      images[i].src = `http://localhost:8081/images/${images[i].basepath}`
-      images[i].thumbnail = `http://localhost:8081/images/${images[i].basepath}`
-      images[i].thumbnailWidth = images[i].width || 16
-      images[i].thumbnailHeight = images[i].height || 9
+      images[i].src = `http://localhost:8081${images[i].paths['1024x640']}`
+      images[i].thumbnail = `http://localhost:8081${images[i].paths['180h']}`
+      images[i].thumbnailWidth = images[i].width
+      images[i].thumbnailHeight = images[i].height
+      images[i].tags = [{'title': 'comment', 'value': images[i].created_at}]
       delete images[i].basepath
+      delete images[i].path
       delete images[i].lat
       delete images[i].lon
       delete images[i].width
@@ -47,7 +50,7 @@ export function imagesFail() {
 export function fetchImages(username, trip) {
   return {
     [CALL_API]: {
-      endpoint: `http://localhost:8081/api/images/image/${username}/${trip}`,
+      endpoint: `http://localhost:8081/api/images/image/${username}/${trip}?size=100`,
       method: 'GET',
       types: [REQUEST_IMAGES, 
 			{
--- src/components/PhotoTab.js
+++ src/components/PhotoTab.js
@@ -17,41 +17,18 @@ class PhotoTab extends Component {
 	render() {
     const { username, tripid, isFetching } = this.props
 		if (isFetching) return (
-      <div className='button is-info is-loading' style={{width: '100%', height: '270px'}}>
+      <div className='button is-info is-loading' style={{width: '100%', height: '320px'}}>
       <h3>Loading the trip map, hold tight...</h3></div>
     )
     const images = this.props.images
     if (!images) return (
       <h3>No images to display</h3>
     )
-		const IMAGES = [
-			{
-			src: "http://localhost:8081/images/dan/7/e333c812-fe0d-5ba5-b2de-6a54b4126fe3.jpg",
-			thumbnail: "http://localhost:8081/images/dan/7/e333c812-fe0d-5ba5-b2de-6a54b4126fe3.jpg",
-			thumbnailWidth: 6000,
-      thumbnailHeight: 2000
-			},
-			{
-			src: "http://localhost:8081/images/dan/7/a42165ab-f559-5f66-abf9-3ae8afe67334.jpg",
-			thumbnail: "http://localhost:8081/images/dan/7/a42165ab-f559-5f66-abf9-3ae8afe67334.jpg",
-			 tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-			width: 600,
-			height: 100,
-		caption: "Boats (Jeshu John - designerspics.com)"
-			}
-		]
 		return (
-      <div>
-        <div className="tile is-ancestor">
-					<div className="tile is-parent">
-						<div className="tile is-child">
-							<Gallery
-								images={images}
-								rowHeight={240}
-								enableImageSelection={false}/>
-						</div>
-					</div>
-				</div>
+      <div className="content">
+        <Gallery
+          images={images}
+          enableImageSelection={false}/>
       </div>
 		)
 	}
